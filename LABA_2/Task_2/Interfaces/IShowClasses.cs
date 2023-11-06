a86bab68697d910e862dd71e9a37b1736531456a
Dmitry <dima2323fr3@gmail.com>
2023-11-07 02:36:36 +0700

    dfsds

---
+++ LABA_2/Task_2/Interfaces/IShowClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA_2.Task_2.Interfaces
+{
+    internal interface IShowClasses
+    {
+    }
+}
